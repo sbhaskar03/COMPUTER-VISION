a1dfc286dad9f63913efd0d7ce994d6ee9252847
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-30 14:41:54 +0530

    Create program40.py

---
+++ program40.py
@@ -0,0 +1,9 @@
+import cv2
+img = cv2.imread(r"C:\Users\mainu\Pictures\ani5.jpg")
+x, y = 100, 100
+width, height = 200, 150
+roi = img[y:y+height, x:x+width]
+cv2.imshow('ROI', roi)
+cv2.imshow('Original Image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

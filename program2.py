fbc56f3c60a6e0a53309bcb064ecd2687fdff9a8
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-31 09:20:52 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,5 @@
 import cv2
-image_path = r"C:\Users\mainu\Pictures\7304.jpg"
+image_path = r"C:\Users\bhask\Downloads\wallpaperflare.com_wallpaper.jpg"
 image = cv2.imread(image_path)
 blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
 cv2.imshow('Original Image', image)

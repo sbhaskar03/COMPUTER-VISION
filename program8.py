5769ae332fbc5d306f9f36be68f6f5a330867392
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-31 09:31:24 +0530

    Update program8.py

--- program8.py
+++ program8.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
-img = cv2.imread(r"C:\Users\mainu\Pictures\7304.jpg",cv2.IMREAD_COLOR)
+img = cv2.imread(r"C:\Users\bhask\Downloads\wallpaperflare.com_wallpaper.jpg",cv2.IMREAD_COLOR)
 img = cv2.resize(img,(600,600))
 cv2.imshow("image",img)
 cv2.waitKey(0)

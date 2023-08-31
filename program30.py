882aefcd5b376b4a8f06fdf3938de48489910a81
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-31 09:26:50 +0530

    Update program30.py

--- program30.py
+++ program30.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread(r"C:\Users\mainu\Pictures\7304.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(r"C:\Users\bhask\Downloads\wallpaperflare.com_wallpaper.jpg", cv2.IMREAD_GRAYSCALE)
 kernel = np.ones((5,5), np.uint8)
 dilation = cv2.dilate(img, kernel, iterations=1)
 cv2.imshow("Original", img)

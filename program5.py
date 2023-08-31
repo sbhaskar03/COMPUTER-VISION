6c2744f19592187c4d3aa6994d57e0adbfc1c73f
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-31 09:30:23 +0530

    Update program5.py

--- program5.py
+++ program5.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = r"C:\Users\mainu\Pictures\7304.jpg"
+path = r"C:\Users\bhask\Downloads\wallpaperflare.com_wallpaper.jpg"
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)

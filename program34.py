9c5e170a150fe2109868b2642d815c6f50be3dd3
sbhaskar03 <112234636+sbhaskar03@users.noreply.github.com>
2023-08-31 09:28:13 +0530

    Update program34.py

--- program34.py
+++ program34.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread(r"C:\Users\mainu\Pictures\7304.jpg",cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(r"C:\Users\bhask\Downloads\wallpaperflare.com_wallpaper.jpg",cv2.IMREAD_GRAYSCALE)
 kernel = np.ones((5,5), np.uint8)
 tophat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
 cv2.imshow("Original", img)
